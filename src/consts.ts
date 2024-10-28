88b39d24725fe28f4a18d489aa153f82cb3ad79b
jam.jar <darkestknight3d@outlook.com>
2024-10-27 19:43:20 -0700

    personalized site

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "jamdotjar";
+
+export const SITE_DESCRIPTION = 'my blog/website for whatever I want to put on it';
